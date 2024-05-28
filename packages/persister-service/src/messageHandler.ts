b078687129f9a3620c27bc6f3dd3ab966856d6ff
borgesis95 <salvatore.borgesi@pagopa.it>
2024-05-28 16:42:32 +0200

    udpated info on messageHandler

--- packages/persister-service/src/messageHandler.ts
+++ packages/persister-service/src/messageHandler.ts
@@ -1,8 +1,9 @@
-import { SQS } from "signalhub-commons";
+import { SQS, logger } from "signalhub-commons";
 import { SignalEvent } from "./models/domain/models.js";
 import { storeSignalServiceBuilder } from "./services/storeSignal.service.js";
 
 const storeSignalService = storeSignalServiceBuilder();
+const loggerInstance = logger({});
 
 export function processMessage(): (message: SQS.Message) => Promise<void> {
   return async (message: SQS.Message): Promise<void> => {
@@ -11,12 +12,11 @@ export function processMessage(): (message: SQS.Message) => Promise<void> {
     const signalEvent = SignalEvent.safeParse(JSON.parse(message.Body!));
 
     if (!signalEvent.success) {
-      const invalidEnvVars = signalEvent.error.issues.flatMap(
-        (issue) => issue.path
+      const invalidSignalEventVars = signalEvent.error.issues.flatMap(
+        (issue) => `${issue.path}: ${issue.message}`
       );
-
-      console.error(
-        "Invalid or missing env vars: " + invalidEnvVars.join(", ")
+      loggerInstance.error(
+        "Invalid Signal event: " + invalidSignalEventVars.join(", ")
       );
     } else {
       storeSignalService.storeSignal(signalEvent.data);
