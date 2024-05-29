b4ea6c5b096d257c612656ce215c40e7f58dfa4b
Paolo Manca <paolo.manca@pagopa.it>
2024-05-29 09:41:40 +0200

    middleware custom with high order function

--- packages/push-signal-service/src/authorization/authorization.middleware.ts
+++ packages/push-signal-service/src/authorization/authorization.middleware.ts
@@ -6,41 +6,39 @@ import { SignalService } from "../services/signal.service.js";
 
 const makeApiProblem = makeApiProblemBuilder({});
 
-export const authorizationMiddleware = async (
-  req: Request,
-  response: Response,
-  next: NextFunction,
-  signalService: SignalService
-): Promise<void> => {
-  const loggerInstance = logger({
-    serviceName: req.ctx.serviceName,
-    correlationId: req.ctx.correlationId,
-  });
-  if (process.env.SKIP_AUTH_VERIFICATION === true) {
-    loggerInstance.info("Authorization SKIP");
-    return next();
-  }
-  try {
-    loggerInstance.info("Authorization BEGIN");
-    const producerId = await producerHasAgreementWithPushSignalEService(
-      req.ctx.sessionData.purposeId
-    );
-    const { eserviceId } = req.body;
+export const authorizationMiddleware = (signalService: SignalService) => {
+  return async (req: Request, response: Response, next: NextFunction) => {
+    console.log("here I am");
+    const loggerInstance = logger({
+      serviceName: req.ctx.serviceName,
+      correlationId: req.ctx.correlationId,
+    });
+    if (process.env.SKIP_AUTH_VERIFICATION) {
+      loggerInstance.info("Authorization SKIP");
+      return next();
+    }
+    try {
+      loggerInstance.info("Authorization BEGIN");
+      const producerId = await producerHasAgreementWithPushSignalEService(
+        req.ctx.sessionData.purposeId
+      );
+      const { eserviceId } = req.body;
 
-    await signalService.isOwned(producerId, eserviceId, loggerInstance);
-    loggerInstance.info("Authorization END");
-    next();
-  } catch (error) {
-    const problem = makeApiProblem(
-      error,
-      (err) =>
-        match(err.code)
-          .with("unauthorizedError", () => 401)
-          .with("operationForbidden", () => 403)
-          .otherwise(() => 500),
-      loggerInstance,
-      req.ctx.correlationId
-    );
-    response.status(problem.status).json(problem).end();
-  }
+      await signalService.isOwned(producerId, eserviceId, loggerInstance);
+      loggerInstance.info("Authorization END");
+      next();
+    } catch (error) {
+      const problem = makeApiProblem(
+        error,
+        (err) =>
+          match(err.code)
+            .with("unauthorizedError", () => 401)
+            .with("operationForbidden", () => 403)
+            .otherwise(() => 500),
+        loggerInstance,
+        req.ctx.correlationId
+      );
+      response.status(problem.status).json(problem).end();
+    }
+  };
 };
