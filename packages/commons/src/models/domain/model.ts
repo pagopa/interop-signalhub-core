52a2253c2faedc148ddd970773bc3dd551927c59
Paolo Manca <paolo.manca@pagopa.it>
2024-06-03 17:22:44 +0200

    moving type root to common

--- packages/commons/src/models/domain/model.ts
+++ packages/commons/src/models/domain/model.ts
@@ -1,5 +1,43 @@
 import { z } from "zod";
 
+const StandardJWTClaims = z.object({
+  // All standard claims except "sub", which is not present in UI tokens
+  iss: z.string(),
+  aud: z.string(),
+  exp: z.number(),
+  nbf: z.number(),
+  iat: z.number(),
+  jti: z.string(),
+});
+
+export const AuthToken = StandardJWTClaims.merge(
+  z.object({
+    purposeId: z.string().uuid(),
+    client_id: z.string().uuid(),
+    sub: z.string(),
+  })
+);
+export type AuthToken = z.infer<typeof AuthToken>;
+
+export const SessionData = z.object({
+  purposeId: z.string().uuid(),
+});
+export type SessionData = z.infer<typeof SessionData>;
+
+export const AppContext = z.object({
+  serviceName: z.string(),
+  correlationId: z.string(),
+  sessionData: SessionData,
+});
+
+export const Headers = z.object({
+  authorization: z.string().nullish(),
+  "x-correlation-id": z.string().nullish(),
+});
+
+export type Headers = z.infer<typeof Headers>;
+export type AppContext = z.infer<typeof AppContext>;
+
 export const SignalType = z.enum(["CREATE", "UPDATE", "DELETE", "SEEDUPDATE"]);
 
 export const SignalSchema = z.object({
--- packages/commons/src/models/types.ts
+++ packages/commons/src/models/types.ts
@@ -1,4 +1,4 @@
-import { AppContext } from "../commons";
+import { AppContext } from "./domain/model.js";
 
 declare global {
   namespace Express {
