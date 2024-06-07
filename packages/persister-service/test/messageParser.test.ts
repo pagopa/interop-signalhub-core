f7d8867b83f71d308b0d012a4c096f23999f6387
Paolo Manca <paolo.manca@pagopa.it>
2024-06-07 12:22:50 +0200

    testing parsing queue message

--- packages/persister-service/test/messageParser.test.ts
+++ packages/persister-service/test/messageParser.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest";
-import { processMessage } from "../src/messageHandler.js";
-import { notRecoverableGenericMessageError } from "../src/models/domain/errors.js";
+import {
+  notRecoverableGenericMessageError,
+  notRecoverableMessageError,
+} from "../src/models/domain/errors.js";
 import { parseQueueMessageToSignal } from "../src/models/domain/utils.js";
 import { genericLogger } from "signalhub-commons";
 
@@ -17,5 +19,127 @@ describe("Message parser", () => {
         malformedNotASignalQueueMessage.Body
       )
     );
+  });
+
+  it("should throw an error if message is malformed Signal (wrong signalId)", () => {
+    const malformedSignalQueueMessage = `
+    {
+      "signalId": "WRONG! WRONG! WRONG!",
+      "eserviceId": "eservice-id-test",
+      "objectId": "object-id-test",
+      "objectType": "object-type-test",
+      "correlationId": "correlation-id-test-1",
+      "signalType": "CREATE"
+    }
+    `;
+    expect(() =>
+      parseQueueMessageToSignal(
+        {
+          Body: malformedSignalQueueMessage,
+        },
+        genericLogger
+      )
+    ).toThrowError(
+      notRecoverableMessageError(
+        "parsingError",
+        JSON.parse(malformedSignalQueueMessage)
+      )
+    );
+  });
+  it("should throw an error if message is malformed Signal (signalId as string) ", () => {
+    const malformedSignalQueueMessage = `
+    {
+      "signalId": "1",
+      "eserviceId": "eservice-id-test",
+      "objectId": "object-id-test",
+      "objectType": "object-type-test",
+      "correlationId": "correlation-id-test-1",
+      "signalType": "CREATE"
+    }
+    `;
+    expect(() =>
+      parseQueueMessageToSignal(
+        {
+          Body: malformedSignalQueueMessage,
+        },
+        genericLogger
+      )
+    ).toThrowError(
+      notRecoverableMessageError(
+        "parsingError",
+        JSON.parse(malformedSignalQueueMessage)
+      )
+    );
+  });
+  it("should throw an error if message is malformed Signal (no  attribute eserviceId)", () => {
+    const malformedSignalQueueMessage = `
+    {
+      "signalId": "1",
+      "objectId": "object-id-test",
+      "objectType": "object-type-test",
+      "correlationId": "correlation-id-test-1",
+      "signalType": "CREATE"
+    }
+    `;
+    expect(() =>
+      parseQueueMessageToSignal(
+        {
+          Body: malformedSignalQueueMessage,
+        },
+        genericLogger
+      )
+    ).toThrowError(
+      notRecoverableMessageError(
+        "parsingError",
+        JSON.parse(malformedSignalQueueMessage)
+      )
+    );
+  });
+  it("should throw an error if message is malformed Signal (adding  arbitrary attribute)", () => {
+    const malformedSignalQueueMessage = `
+    {
+      "signalId": "1",
+      "eserviceId": "eservice-id-test",
+      "objectId": "object-id-test",
+      "objectType": "object-type-test",
+      "correlationId": "correlation-id-test-1",
+      "signalType": "CREATE",
+      "arbitrary": "no way!"
+    }
+    `;
+    expect(() =>
+      parseQueueMessageToSignal(
+        {
+          Body: malformedSignalQueueMessage,
+        },
+        genericLogger
+      )
+    ).toThrowError(
+      notRecoverableMessageError(
+        "parsingError",
+        JSON.parse(malformedSignalQueueMessage)
+      )
+    );
+  });
+
+  it("should parse message to correct type Signal", () => {
+    const correctSignalQueueMessage = `
+    {
+      "signalId": 1,
+      "eserviceId": "eservice-id-test",
+      "objectId": "object-id-test",
+      "objectType": "object-type-test",
+      "correlationId": "correlation-id-test-1",
+      "signalType": "CREATE"
+    }
+    `;
+    expect(
+      parseQueueMessageToSignal(
+        {
+          Body: correctSignalQueueMessage,
+        },
+        genericLogger
+      )
+    ).toEqual(JSON.parse(correctSignalQueueMessage));
   });
 });
