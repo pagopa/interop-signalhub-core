d629cd30dce193428121daef33c4300d3a7e1c33
Paolo Manca <paolo.manca@pagopa.it>
2024-06-14 11:26:32 +0200

    fix script

--- packages/pull-signal-service/src/routes/swagger.route.ts
+++ packages/pull-signal-service/src/routes/swagger.route.ts
@@ -1,6 +1,6 @@
 import * as swaggerUi from "swagger-ui-express";
 import { Express } from "express";
-import { openApiDocument } from "../scripts/generate-interface.js";
+import { openApiDocument } from "../openapi-script/generate-interface.js";
 
 export const setupSwaggerRoute = (app: Express): void => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
