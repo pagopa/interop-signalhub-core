40e8a9ed17a3120983302add486e7f3a8115f312
Paolo Manca <paolo.manca@pagopa.it>
2024-06-16 23:41:27 +0200

    change logic api client

--- packages/pull-signal-service/src/services/interopApiClient.service.ts
+++ packages/pull-signal-service/src/services/interopApiClient.service.ts
@@ -4,9 +4,16 @@ import { Agreement } from "../model/domain/models.js";
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function interopApiClientServiceBuilder() {
   return {
-    async getAgreementByPurposeId(purposeId: string): Promise<Agreement> {
-      const { data } = await getAgreementByPurpose(purposeId);
-      return data as unknown as Agreement;
+    async getAgreementByPurposeId(
+      purposeId: string
+    ): Promise<Agreement | null> {
+      try {
+        const { data } = await getAgreementByPurpose(purposeId);
+        return data as unknown as Agreement;
+      } catch (error) {
+        // TODO: handle error
+      }
+      return null;
     },
   };
 }
