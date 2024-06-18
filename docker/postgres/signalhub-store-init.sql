491431ed8e094b2a59e4990d752fc1532afac5e4
Paolo Manca <paolo.manca@pagopa.it>
2024-06-18 15:18:48 +0200

    cofigure local environment with docker-compose and create DDL script

--- docker/postgres/signalhub-store-init.sql
+++ docker/postgres/signalhub-store-init.sql
@@ -1,4 +1,7 @@
-CREATE TABLE IF NOT EXISTS ESERVICE (
+create schema if not exists "dev_interop";
+create schema if not exists "dev_signalhub";
+	
+CREATE TABLE IF NOT EXISTS "dev_interop"."eservice" (
     eservice_id     VARCHAR (255) NOT NULL,
     producer_id     VARCHAR (255) NOT NULL,
     descriptor_id   VARCHAR (255) NOT NULL,
@@ -9,11 +12,11 @@ CREATE TABLE IF NOT EXISTS ESERVICE (
     UNIQUE (eservice_id, producer_id, descriptor_id),
     PRIMARY KEY (eservice_id, producer_id, descriptor_id)
 );
-CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_ID ON ESERVICE USING hash (eservice_id);
-CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_PRODUCER_ID ON ESERVICE USING hash (producer_id);
 
+CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_ID ON "dev_interop"."eservice" USING hash (eservice_id);
+CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_PRODUCER_ID ON "dev_interop"."eservice" USING hash (producer_id);
 
-CREATE TABLE IF NOT EXISTS CONSUMER_ESERVICE (
+CREATE TABLE IF NOT EXISTS "dev_interop"."consumer_eservice" (
     agreement_id    VARCHAR (255) NOT NULL,
     eservice_id     VARCHAR (255) NOT NULL,
     consumer_id     VARCHAR (255) NOT NULL,
@@ -25,12 +28,12 @@ CREATE TABLE IF NOT EXISTS CONSUMER_ESERVICE (
     UNIQUE (eservice_id, consumer_id, descriptor_id),
     PRIMARY KEY (eservice_id, consumer_id, descriptor_id)
 );
-CREATE INDEX IF NOT EXISTS CONSUMER_ESERVICE_INDEX_ID ON CONSUMER_ESERVICE USING hash (eservice_id);
-CREATE INDEX IF NOT EXISTS CONSUMER_ESERVICE_INDEX_CONSUMER_ID ON CONSUMER_ESERVICE USING hash (consumer_id);
-CREATE INDEX IF NOT EXISTS CONSUMER_ESERVICE_INDEX_DESCRIPTOR_ID ON CONSUMER_ESERVICE USING hash (descriptor_id);
 
+CREATE INDEX IF NOT EXISTS CONSUMER_ESERVICE_INDEX_ID ON "dev_interop"."consumer_eservice" USING hash (eservice_id);
+CREATE INDEX IF NOT EXISTS CONSUMER_ESERVICE_INDEX_CONSUMER_ID ON "dev_interop"."consumer_eservice" USING hash (consumer_id);
+CREATE INDEX IF NOT EXISTS CONSUMER_ESERVICE_INDEX_DESCRIPTOR_ID ON "dev_interop"."consumer_eservice" USING hash (descriptor_id);
 
-CREATE TABLE IF NOT EXISTS SIGNAL (
+CREATE TABLE IF NOT EXISTS "dev_signalhub"."signal" (
     id             SERIAL PRIMARY KEY,
     correlation_id VARCHAR(255) NOT NULL,
     signal_id      BIGINT        NOT NULL,
@@ -41,11 +44,11 @@ CREATE TABLE IF NOT EXISTS SIGNAL (
     tmst_insert    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (signal_id, eservice_id)
 );
-CREATE INDEX IF NOT EXISTS SIGNAL_INDEX_SIGNAL_ID ON SIGNAL USING hash (signal_id);
-CREATE INDEX IF NOT EXISTS SIGNAL_INDEX_ESERVICE_ID ON SIGNAL USING hash (eservice_id);
 
+CREATE INDEX IF NOT EXISTS SIGNAL_INDEX_SIGNAL_ID ON "dev_signalhub"."signal" USING hash (signal_id);
+CREATE INDEX IF NOT EXISTS SIGNAL_INDEX_ESERVICE_ID on "dev_signalhub"."signal" USING hash (eservice_id);
 
-CREATE TABLE IF NOT EXISTS DEAD_SIGNAL (
+CREATE TABLE IF NOT EXISTS "dev_signalhub"."dead_signal" (
     id             SERIAL PRIMARY KEY,
     correlation_id VARCHAR(255)   NOT NULL,
     signal_id      BIGINT        NOT NULL,
@@ -57,8 +60,7 @@ CREATE TABLE IF NOT EXISTS DEAD_SIGNAL (
     error_reason   VARCHAR(255)  NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS TRACING_BATCH (
+CREATE TABLE IF NOT EXISTS "dev_interop"."tracing_batch" (
     batch_id         SERIAL PRIMARY KEY,
     state            VARCHAR (255) NOT NULL,
     type             VARCHAR (50) NOT NULL,
@@ -66,8 +68,7 @@ CREATE TABLE IF NOT EXISTS TRACING_BATCH (
     tmst_created     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE IF NOT EXISTS DEAD_EVENT (
+CREATE TABLE IF NOT EXISTS "dev_interop"."dead_event" (
     event_tmp_id        SERIAL PRIMARY KEY,
     tmst_insert         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     error_reason        VARCHAR(255) NOT NULL,
