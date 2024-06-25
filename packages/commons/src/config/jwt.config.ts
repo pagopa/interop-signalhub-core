eda2824e9439405f349ecf55c528685d67eb76b6
Paolo Manca <paolo.manca@pagopa.it>
2024-06-25 16:24:06 +0200

    restore well known url

--- packages/commons/src/config/jwt.config.ts
+++ packages/commons/src/config/jwt.config.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 export const JWTConfig = z
   .object({
-    WELL_KNOWN_URLS: z.coerce.string(),
-    ACCEPTED_AUDIENCE: z.coerce.string(),
+    WELL_KNOWN_URL: z.string(),
+    ACCEPTED_AUDIENCE: z.string(),
   })
   .transform((c) => ({
-    wellKnownUrl: c.WELL_KNOWN_URLS,
+    wellKnownUrl: c.WELL_KNOWN_URL,
     acceptedAudience: c.ACCEPTED_AUDIENCE,
   }));
 export type JWTConfig = z.infer<typeof JWTConfig>;
