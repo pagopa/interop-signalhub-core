5fb600a0527560cd1f1e88aaa1c63a53708482a7
borgesis95 <salvatore.borgesi@pagopa.it>
2024-06-25 10:34:39 +0200

    Signalhub-fix-swagger: Removed temporarily swagger routes (#32)

--- packages/pull-signal-service/src/app.ts
+++ packages/pull-signal-service/src/app.ts
@@ -3,7 +3,7 @@ import { initServer, createExpressEndpoints } from "@ts-rest/express";
 import { authenticationMiddleware, contextMiddleware } from "signalhub-commons";
 import { contract } from "./contract/contract.js";
 import { pullRoutes } from "./routes/pull.route.js";
-import { setupSwaggerRoute } from "./routes/swagger.route.js";
+// import { setupSwaggerRoute } from "./routes/swagger.route.js";
 import { validationErrorHandler } from "./validation/validation.js";
 import { serviceBuilder } from "./services/service.builder.js";
 
@@ -16,7 +16,7 @@ const { signalService, interopService } = serviceBuilder();
 const app: Express = express();
 app.use(contextMiddleware(serviceName));
 app.use(authenticationMiddleware);
-setupSwaggerRoute(app);
+// setupSwaggerRoute(app);
 // Disable the "X-Powered-By: Express" HTTP header for security reasons: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
 app.disable("x-powered-by");
 
