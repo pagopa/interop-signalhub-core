2a859b608357cd6a81811ca71b4b4bc761f02430
Paolo Manca <paolo.manca@pagopa.it>
2024-07-03 15:26:52 +0200

    scripting to start infra and application

---
+++ scripts/infra-status.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+__dir="$(cd "$(dirname "$0")" && pwd)"
+docker compose -f $__dir/../docker/docker-compose.yml logs -f -t
---
+++ scripts/pull-start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Function to be executed upon receiving EXIT
+cleanup() {
+    echo "Caught EXIT. Cleaning up..."
+    kill $pid1 # Terminate server process
+    exit
+}
+
+# Set up the trap
+trap cleanup EXIT
+
+# Start push service in the background
+pnpm start:pull-signals &
+pid1=$! # Get the process ID of the last backgrounded command
+
+# Wait indefinitely. The cleanup function will handle interruption and cleanup.
+wait
