8ee24f3ba7af2b6a441647c8616050a1358b7453
Paolo Manca <paolo.manca@pagopa.it>
2024-07-25 15:59:23 +0200

    fix type

--- packages/kafka-connector/src/index.ts
+++ packages/kafka-connector/src/index.ts
@@ -157,7 +157,7 @@ export const initConsumer = async (
         throw kafkaMessageProcessError(
           payload.topic,
           payload.partition,
-          payload.kafkaMessage.offset,
+          payload.message.offset,
           e
         );
       }
@@ -255,12 +255,12 @@ const kafkaCommitMessageOffsets = async (
   consumer: Consumer,
   payload: EachMessagePayload
 ): Promise<void> => {
-  const { topic, partition, kafkaMessage } = payload;
+  const { topic, partition, message } = payload;
   await consumer.commitOffsets([
-    { topic, partition, offset: (Number(kafkaMessage.offset) + 1).toString() },
+    { topic, partition, offset: (Number(message.offset) + 1).toString() },
   ]);
 
   genericLogger.debug(
-    `Topic message offset ${Number(kafkaMessage.offset) + 1} committed`
+    `Topic message offset ${Number(message.offset) + 1} committed`
   );
 };
