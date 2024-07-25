6d15de6b23192cd3e5de403a9e2f7c5b4cd9c170
Paolo Manca <paolo.manca@pagopa.it>
2024-07-25 18:26:46 +0200

    update db schema, fix pinned kafka version

--- docker/postgres/signalhub-store-init.sql
+++ docker/postgres/signalhub-store-init.sql
@@ -3,35 +3,53 @@ create schema if not exists "dev_signalhub";
 	
 CREATE TABLE IF NOT EXISTS "dev_interop"."eservice" (
     eservice_id     VARCHAR (255) NOT NULL,
-    producer_id     VARCHAR (255) NOT NULL,
     descriptor_id   VARCHAR (255) NOT NULL,
-    event_id        BIGINT,
+    producer_id     VARCHAR (255) NOT NULL,
     state           VARCHAR (255) NOT NULL,
+    event_id        BIGINT DEFAULT -1,
+    event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
+    event_version_id   BIGINT NOT NULL DEFAULT -1,
     tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     tmst_last_edit  TIMESTAMPTZ,
-    UNIQUE (eservice_id, producer_id, descriptor_id),
-    PRIMARY KEY (eservice_id, producer_id, descriptor_id)
+    UNIQUE (event_stream_id, event_version_id),
+    UNIQUE (eservice_id, descriptor_id, producer_id),
+    PRIMARY KEY (eservice_id, descriptor_id)
 );
-
-CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_ID ON "dev_interop"."eservice" USING hash (eservice_id);
-CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_PRODUCER_ID ON "dev_interop"."eservice" USING hash (producer_id);
+CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_ID_AND_PRODUCER_AND_STATE ON "dev_interop"."eservice"(eservice_id, producer_id, state);
 
 CREATE TABLE IF NOT EXISTS "dev_interop"."agreement" (
     agreement_id    VARCHAR (255) NOT NULL,
     eservice_id     VARCHAR (255) NOT NULL,
-    consumer_id     VARCHAR (255) NOT NULL,
     descriptor_id   VARCHAR (255) NOT NULL,
-    event_id        BIGINT,
+    consumer_id     VARCHAR (255) NOT NULL,
     state           VARCHAR (255) NOT NULL,
+    event_id        BIGINT DEFAULT -1,
+    event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
+    event_version_id   BIGINT NOT NULL DEFAULT -1,
     tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     tmst_last_edit  TIMESTAMPTZ,
-    UNIQUE (eservice_id, consumer_id, descriptor_id),
-    PRIMARY KEY (eservice_id, consumer_id, descriptor_id)
+    UNIQUE (event_stream_id, event_version_id),
+    UNIQUE (eservice_id, descriptor_id, consumer_id),
+    PRIMARY KEY (agreement_id)
 );
+CREATE INDEX IF NOT EXISTS AGREEMENT_INDEX_ESERVICE_AND_CONSUMER_AND_STATE ON "dev_interop"."agreement"(eservice_id, consumer_id, state);
 
-CREATE INDEX IF NOT EXISTS agreement_INDEX_ID ON "dev_interop"."agreement" USING hash (eservice_id);
-CREATE INDEX IF NOT EXISTS agreement_INDEX_CONSUMER_ID ON "dev_interop"."agreement" USING hash (consumer_id);
-CREATE INDEX IF NOT EXISTS agreement_INDEX_DESCRIPTOR_ID ON "dev_interop"."agreement" USING hash (descriptor_id);
+CREATE TABLE IF NOT EXISTS "dev_interop"."purpose" (
+    purpose_id       VARCHAR (255) NOT NULL,
+    purpose_version_id  VARCHAR (255) NOT NULL,
+    purpose_state    VARCHAR (255) NOT NULL,
+    eservice_id     VARCHAR (255) NOT NULL,
+    consumer_id     VARCHAR (255) NOT NULL,
+    event_id        BIGINT DEFAULT -1,
+    event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
+    event_version_id   BIGINT NOT NULL DEFAULT -1,
+    tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    tmst_last_edit  TIMESTAMPTZ,
+    UNIQUE (event_stream_id, event_version_id),
+    UNIQUE (purpose_id, eservice_id, consumer_id),
+    PRIMARY KEY (purpose_id, purpose_version_id)
+);
+CREATE INDEX IF NOT EXISTS PURPOSE_INDEX_ESERVICE_AND_CONSUMER_AND_STATE ON "dev_interop"."purpose"(purpose_id, consumer_id, purpose_state, eservice_id);
 
 CREATE TABLE IF NOT EXISTS "dev_interop"."tracing_batch" (
     batch_id         SERIAL PRIMARY KEY,
