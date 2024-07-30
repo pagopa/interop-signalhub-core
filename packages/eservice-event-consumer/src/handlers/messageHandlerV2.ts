dd5e7d69178945d33a675610760499fe7e239424
borgesis95 <salvatore.borgesi@pagopa.it>
2024-07-30 14:26:38 +0200

    feat: starting handling message V2

--- packages/eservice-event-consumer/src/handlers/messageHandlerV2.ts
+++ packages/eservice-event-consumer/src/handlers/messageHandlerV2.ts
@@ -1,3 +1,55 @@
-export async function handleMessageV2() {
-  return 1;
+import { EServiceEventV2 } from "@pagopa/interop-outbound-models";
+import { Logger } from "pagopa-signalhub-commons";
+import { P, match } from "ts-pattern";
+
+export async function handleMessageV1(
+  event: EServiceEventV2,
+  logger: Logger
+): Promise<void> {
+  await match(event)
+    .with(
+      {
+        type: "EServiceAdded",
+      },
+      async (evt) => {
+        console.log("TODO", evt);
+      }
+    )
+    .with(
+      {
+        type: P.union(
+          "EServiceDescriptorAdded",
+          "EServiceDescriptorActivated",
+          "EServiceDescriptorArchived",
+          "EServiceDescriptorPublished",
+          "EServiceDescriptorSuspended",
+          "EServiceDraftDescriptorUpdated",
+          "DraftEServiceUpdated",
+          "EServiceDraftDescriptorDeleted" // ??
+        ),
+      },
+      async (evt) => {
+        console.log("TODO", evt);
+      }
+    )
+    .with(
+      {
+        type: "EServiceCloned", //?? ,
+      },
+      async (evt) => {
+        console.log("TODO", evt);
+      }
+    )
+    .with(
+      {
+        type: "EServiceDeleted",
+      },
+      async (evt) => {
+        console.log("TODO", evt);
+      }
+    )
+
+    .otherwise(async () => {
+      logger.debug(`Event type ${event.type} not relevant`);
+    });
 }
