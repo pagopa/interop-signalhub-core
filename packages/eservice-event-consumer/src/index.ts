8d527e92109e3e1e2404518576e86f1077046bcc
borgesis95 <salvatore.borgesi@pagopa.it>
2024-08-08 09:38:14 +0200

    fix: lint error

--- packages/eservice-event-consumer/src/index.ts
+++ packages/eservice-event-consumer/src/index.ts
@@ -15,31 +15,8 @@ export async function processMessage({
   message,
   partition,
 }: EachMessagePayload): Promise<void> {
-  try {
-    if (!message.value) {
-      throw kafkaMissingMessageValue(config.kafkaTopic);
-    }
-    const eserviceEvent = decodeOutboundEServiceEvent(message.value.toString());
-
-    const logger = buildLoggerInstance(serviceName, eserviceEvent);
-    logger.info(
-      `Processing message event: ${eserviceEvent.stream_id}/${eserviceEvent.version}`
-    );
-
-    await match(eserviceEvent)
-      .with({ event_version: 1 }, (event) =>
-        handleMessageV1(event, eServiceService, logger)
-      )
-      .with({ event_version: 2 }, (event) =>
-        handleMessageV2(event, eServiceService, logger)
-      )
-      .exhaustive();
-
-    logger.info(
-      `Message was processed. Partition number: ${partition}. Offset: ${message.offset}`
-    );
-  } catch (error) {
-    throw error;
+  if (!message.value) {
+    throw kafkaMissingMessageValue(config.kafkaTopic);
   }
   const eserviceEvent = decodeOutboundEServiceEvent(message.value.toString());
 
