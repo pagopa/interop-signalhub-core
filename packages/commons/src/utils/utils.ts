95672840b769bbb877610b012dc0966b80620f4a
borgesis95 <salvatore.borgesi@pagopa.it>
2024-09-04 17:52:49 +0200

    NTRP-304: Add skipForUrl on authenticationMiddleware (#121)

--- packages/commons/src/utils/utils.ts
+++ packages/commons/src/utils/utils.ts
@@ -1,3 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+
 export function getCurrentDate(): string {
   return new Date().toISOString();
+}
+
+type MiddlewareFn = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void | Response>;
+
+export function skipForUrl(
+  path: string,
+  middleware: MiddlewareFn
+): (req: Request, res: Response, next: NextFunction) => void {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (path === req.path) {
+      return next();
+    } else {
+      return middleware(req, res, next);
+    }
+  };
 }
