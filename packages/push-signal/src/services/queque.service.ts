3d39d2ab27f81e50473605e140c5ad30ea759a94
Paolo Manca <paolo.manca@pagopa.it>
2024-09-05 17:42:28 +0200

    fix error

--- packages/push-signal/src/services/queque.service.ts
+++ packages/push-signal/src/services/queque.service.ts
@@ -16,8 +16,9 @@ export function queueServiceBuilder(sqsClient: SQS.SQSClient) {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (error: any) {
         logger.warn(`QuequeService::send message not sent: ${error}`);
-        const { requestId } = error.$metadata;
-        throw signalNotSendedToQueque(requestId, error);
+        // eslint-disable-next-line functional/no-let
+        const requestId = error.$metadata ? error.$metadata.requestId : null;
+        throw signalNotSendedToQueque(error, requestId);
       }
     },
   };
