ca768f6e321ac580bd70ae0f5a189853d00b8777
Paolo Manca <paolo.manca@pagopa.it>
2024-09-09 16:52:37 +0200

    change db structure and update index

--- docker/postgres/signalhub-store-init.sql
+++ docker/postgres/signalhub-store-init.sql
@@ -1,67 +1,58 @@
 create schema if not exists "dev_interop";
 create schema if not exists "dev_signalhub";
 
-
-
 CREATE TABLE IF NOT EXISTS "dev_interop"."eservice_producer" (
-    eservice_id     VARCHAR (255) NOT NULL,
+    eservice_id     VARCHAR (255) PRIMARY KEY,
     producer_id     VARCHAR (255) NOT NULL,
     tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
-    event_version_id   BIGINT NOT NULL DEFAULT -1,
-    UNIQUE (event_stream_id, event_version_id),
-    UNIQUE (eservice_id, producer_id),
-    PRIMARY KEY (eservice_id)
-); 
+    event_version_id BIGINT NOT NULL DEFAULT -1
+);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_ESERVICE_PRODUCER_ON_EVENT_STREAM_ID_VERSION_ID  ON "dev_interop"."eservice_producer"(event_stream_id, event_version_id);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_ESERVICE_PRODUCER_ON_ESERVICE_ID_PRODUCER_ID  ON "dev_interop"."eservice_producer"(eservice_id, producer_id);
 	
 CREATE TABLE IF NOT EXISTS "dev_interop"."eservice" (
     eservice_id     VARCHAR (255) NOT NULL,
     descriptor_id   VARCHAR (255) NOT NULL,
     producer_id     VARCHAR (255) NOT NULL,
-    state           VARCHAR (255) NOT NULL,
-    event_id        BIGINT DEFAULT -1,
+    "state"           VARCHAR (255) NOT NULL,
     event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
     event_version_id   BIGINT NOT NULL DEFAULT -1,
     tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     tmst_last_edit  TIMESTAMPTZ,
-    UNIQUE (event_stream_id, event_version_id,descriptor_id),
-    UNIQUE (eservice_id, descriptor_id, producer_id),
     PRIMARY KEY (eservice_id, descriptor_id)
 );
-CREATE INDEX IF NOT EXISTS ESERVICE_INDEX_ID_AND_PRODUCER_AND_STATE ON "dev_interop"."eservice"(eservice_id, producer_id, state);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_ESERVICE_ON_EVENT_STREAM_ID_VERSION_ID_DESCRIPTOR_ID  ON "dev_interop"."eservice"(event_stream_id, event_version_id, descriptor_id);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_ESERVICE_ON_ESERVICE_DESCRIPTOR_PRODUCER ON "dev_interop"."eservice"(eservice_id, descriptor_id, producer_id);
+CREATE INDEX IF NOT EXISTS IDX_ESERVICE_ON_ESERVICE_ID_AND_PRODUCER_AND_STATE ON "dev_interop"."eservice"(eservice_id, producer_id, upper(state));
 
 CREATE TABLE IF NOT EXISTS "dev_interop"."agreement" (
-    agreement_id    VARCHAR (255) NOT NULL,
+    agreement_id    VARCHAR (255) PRIMARY KEY,
     eservice_id     VARCHAR (255) NOT NULL,
     descriptor_id   VARCHAR (255) NOT NULL,
     consumer_id     VARCHAR (255) NOT NULL,
-    state           VARCHAR (255) NOT NULL,
-    event_id        BIGINT DEFAULT -1,
+    "state"           VARCHAR (255) NOT NULL,
     event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
     event_version_id   BIGINT NOT NULL DEFAULT -1,
     tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    tmst_last_edit  TIMESTAMPTZ,
-    UNIQUE (event_stream_id, event_version_id),
-    PRIMARY KEY (agreement_id)
+    tmst_last_edit  TIMESTAMPTZ
 );
-CREATE INDEX IF NOT EXISTS AGREEMENT_INDEX_ESERVICE_AND_CONSUMER_AND_STATE ON "dev_interop"."agreement"(eservice_id, consumer_id, state);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_AGREEMENT_ON_EVENT_STREAM_ID_VERSION_ID  ON "dev_interop"."agreement"(event_stream_id, event_version_id);
+CREATE INDEX IF NOT EXISTS IDX_AGREEMENT_ON_ESERVICE_CONSUMER_AND_STATE ON "dev_interop"."agreement"(eservice_id, consumer_id, upper(state));
 
 CREATE TABLE IF NOT EXISTS "dev_interop"."purpose" (
-    purpose_id       VARCHAR (255) NOT NULL,
+    purpose_id       VARCHAR (255) PRIMARY KEY,
     purpose_version_id  VARCHAR (255),
     purpose_state    VARCHAR (255) NOT NULL,
     eservice_id     VARCHAR (255) NOT NULL,
     consumer_id     VARCHAR (255) NOT NULL,
-    event_id        BIGINT DEFAULT -1,
     event_stream_id VARCHAR (255) NOT NULL DEFAULT gen_random_uuid(),
     event_version_id   BIGINT NOT NULL DEFAULT -1,
     tmst_insert     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    tmst_last_edit  TIMESTAMPTZ,
-    UNIQUE (event_stream_id, event_version_id),
-    UNIQUE (purpose_id, eservice_id, consumer_id),
-    PRIMARY KEY (purpose_id)
+    tmst_last_edit  TIMESTAMPTZ
 );
-CREATE INDEX IF NOT EXISTS PURPOSE_INDEX_ESERVICE_AND_CONSUMER_AND_STATE ON "dev_interop"."purpose"(purpose_id, consumer_id, purpose_state, eservice_id);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_PURPOSE_ON_EVENT_STREAM_ID_VERSION_ID  ON "dev_interop"."purpose"(event_stream_id, event_version_id);
+CREATE INDEX IF NOT EXISTS IDX_PURPOSE_ON_PURPOSE_ID_CONSUMER_ID_PURPOSE_STATE_ESERVICE_ID ON "dev_interop"."purpose"(purpose_id, consumer_id, upper(purpose_state), eservice_id);
 
 CREATE TABLE IF NOT EXISTS "dev_signalhub"."signal" (
     id             SERIAL PRIMARY KEY,
@@ -71,12 +62,11 @@ CREATE TABLE IF NOT EXISTS "dev_signalhub"."signal" (
     eservice_id    VARCHAR (255)  NOT NULL,
     object_type    VARCHAR (255)  NOT NULL,
     signal_type    VARCHAR (255)  NOT NULL,
-    tmst_insert    TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (signal_id, eservice_id)
+    tmst_insert    TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE INDEX IF NOT EXISTS SIGNAL_INDEX_SIGNAL_ID ON "dev_signalhub"."signal" USING hash (signal_id);
-CREATE INDEX IF NOT EXISTS SIGNAL_INDEX_ESERVICE_ID on "dev_signalhub"."signal" USING hash (eservice_id);
+CREATE INDEX IF NOT EXISTS IDX_SIGNAL_ON_SIGNAL_ID ON "dev_signalhub"."signal"(signal_id);
+CREATE INDEX IF NOT EXISTS IDX_SIGNAL_ON_ESERVICE_ID on "dev_signalhub"."signal"(eservice_id);
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_SIGNAL_ON_SIGNAL_ID_ESERVICE_ID ON "dev_signalhub"."signal"(eservice_id, signal_id);
 
 CREATE TABLE IF NOT EXISTS "dev_signalhub"."dead_signal" (
     id             SERIAL PRIMARY KEY,
@@ -91,7 +81,7 @@ CREATE TABLE IF NOT EXISTS "dev_signalhub"."dead_signal" (
 );
 
 CREATE TABLE IF NOT EXISTS "dev_signalhub"."tracing_batch_cleanup" (
-    batch_id         SERIAL PRIMARY KEY,
+    batch_id         SERIAL PRIMARY KEY ,
     tmst_start_at    TIMESTAMPTZ,
     tmst_end_at      TIMESTAMPTZ,
     error            VARCHAR (255),
