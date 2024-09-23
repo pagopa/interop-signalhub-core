3041d54c791563e26d5852db55b86a4885c6a569
borgesis95 <salvatore.borgesi@pagopa.it>
2024-09-23 16:21:35 +0200

    feat: added new module for parsing ts-rest on openApi with components

--- packages/push-signal/src/commands/generate-api.command.ts
+++ packages/push-signal/src/commands/generate-api.command.ts
@@ -1,10 +1,61 @@
 import { writeFileSync } from "fs";
-import { generateOpenApi } from "@ts-rest/open-api";
+// import { generateOpenApi } from "@ts-rest/open-api";
+import {
+  // generateComponentFromContractOpenApi,
+  generateOpenApiSpecification,
+} from "pagopa-tsrest-openapi-parser";
 import * as yaml from "js-yaml";
 import { contract } from "../contract/contract.js";
 
 export function generateApi(version: string): void {
-  const openApiDocument = generateOpenApi(
+  // const openApiDocument = generateOpenApi(
+  //   contract,
+  //   {
+  //     components: {
+  //       securitySchemes: {
+  //         bearerAuth: {
+  //           type: "http",
+  //           scheme: "bearer",
+  //           bearerFormat: "JWT",
+  //           description:
+  //             "A bearer token in the format of a JWS and conformed to the specifications included in [RFC8725](https://tools.ietf.org/html/RFC8725).",
+  //         },
+  //       },
+  //     },
+
+  //     security: [
+  //       {
+  //         bearerAuth: [],
+  //       },
+  //     ],
+  //     servers: [
+  //       {
+  //         url: "/signals",
+  //         description: "Push signal data",
+  //       },
+  //     ],
+  //     info: {
+  //       title: "Push signal Service API",
+  //       version,
+  //       license: {
+  //         name: "ISC",
+  //         url: "https://opensource.org/license/isc-license-txt",
+  //       },
+  //       contact: {
+  //         name: "PagoPA support",
+  //         url: "https://github.com/pagopa/interop-signalhub-core/issues",
+  //         email: "Interop-sprint@pagopa.it",
+  //       },
+  //       termsOfService:
+  //         "https://docs.pagopa.it/interoperabilita-1/normativa-e-approfondimenti",
+  //     },
+  //   },
+  //   {
+  //     setOperationId: true,
+  //   }
+  // );
+
+  const openApiDocumentV2 = generateOpenApiSpecification(
     contract,
     {
       components: {
@@ -50,7 +101,6 @@ export function generateApi(version: string): void {
       setOperationId: true,
     }
   );
-
-  const fileOutputDocument = `./src/api/push-signals_${openApiDocument.info.version}_.yaml`;
-  writeFileSync(fileOutputDocument, yaml.dump(openApiDocument));
+  const fileOutputDocument = `./src/api/push-signals_${openApiDocumentV2.info.version}_.yaml`;
+  writeFileSync(fileOutputDocument, yaml.dump(openApiDocumentV2));
 }
---
+++ packages/tsrest-openapi-parser/src/index.ts
@@ -0,0 +1,212 @@
+import {
+  OpenAPIRegistry,
+  OpenApiGeneratorV3,
+  RouteConfig,
+  extendZodWithOpenApi,
+} from "@asteasolutions/zod-to-openapi";
+import {
+  AppRoute,
+  AppRouteResponse,
+  AppRouter,
+  ContractAnyType,
+  isZodType,
+} from "@ts-rest/core";
+// import * as yaml from "js-yaml";
+// import { writeFileSync } from "fs";
+import { z } from "zod";
+import { InfoObject, OpenAPIObject, OperationObject } from "openapi3-ts";
+import { RouteParameter } from "@asteasolutions/zod-to-openapi/dist/openapi-registry.js";
+
+type RouterPath = {
+  id: string;
+  path: string;
+  route: AppRoute;
+  paths: string[];
+};
+
+extendZodWithOpenApi(z);
+
+const mapMethod = {
+  GET: "get",
+  POST: "post",
+  PUT: "put",
+  DELETE: "delete",
+  PATCH: "patch",
+};
+const registry = new OpenAPIRegistry();
+
+export const generateComponentFromContractOpenApi = (
+  router: AppRouter,
+  options: {
+    setOperationId?: boolean | "concatenated-path";
+    jsonQuery?: boolean;
+    operationMapper?: (
+      operation: OperationObject,
+      appRoute: AppRoute
+    ) => OperationObject;
+  } = {}
+) => {
+  const paths = getPathsFromRouter(router);
+
+  const operationIds = new Map<string, string[]>();
+
+  // For each patch i have to register a path...
+
+  paths.forEach((path) => {
+    // --- Check operationId ---
+    if (options.setOperationId === true) {
+      const existingOp = operationIds.get(path.id);
+      if (existingOp) {
+        throw new Error(
+          `Route '${path.id}' already defined under ${existingOp.join(".")}`
+        );
+      }
+      operationIds.set(path.id, path.paths);
+    }
+    // --- End check operationId ---
+
+    const headers = getHeaders(path.route.headers);
+    const responses = getResponses(path.route.responses);
+
+    const body = path.route.method !== "GET" ? path.route.body : null;
+
+    const routeConfigPath: RouteConfig = {
+      description: path.route.description,
+      deprecated: path.route.deprecated,
+      summary: path.route.summary,
+      method: mapMethod[path.route.method] as RouteConfig["method"],
+      request: {
+        body: body
+          ? {
+              content: {
+                "application/json": {
+                  schema: body as any,
+                },
+              },
+              description: "",
+              required: true,
+            }
+          : undefined,
+
+        query: path.route.query as RouteParameter,
+        headers: headers as RouteParameter,
+        params: path.route.pathParams as RouteParameter,
+      },
+      ...(options.setOperationId
+        ? {
+            operationId:
+              options.setOperationId === "concatenated-path"
+                ? [...path.paths, path.id].join(".")
+                : path.id,
+          }
+        : {}),
+      path: path.path,
+      responses,
+    };
+
+    registry.registerPath(routeConfigPath);
+  });
+};
+export function generateOpenApiSpecification(
+  router: AppRouter,
+  apiDoc: Omit<OpenAPIObject, "paths" | "openapi"> & { info: InfoObject },
+  options: {
+    setOperationId?: boolean | "concatenated-path";
+    jsonQuery?: boolean;
+    operationMapper?: (
+      operation: OperationObject,
+      appRoute: AppRoute
+    ) => OperationObject;
+  } = {}
+): OpenAPIObject {
+  generateComponentFromContractOpenApi(router, options);
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+
+  const apiDocuments = generator.generateDocument({
+    openapi: "3.0.0",
+    ...apiDoc,
+  });
+
+  return apiDocuments as OpenAPIObject;
+
+  //   const fileOutputDocument = `./pull-signals_${apiDocuments.info.version}_.yaml`;
+  //   writeFileSync(fileOutputDocument, yaml.dump(apiDocuments));
+}
+
+const getHeaders = (
+  headers: ContractAnyType | undefined
+): RouteParameter | undefined => {
+  return headers && Object.keys(headers).length === 0
+    ? undefined
+    : (headers as RouteParameter);
+};
+
+const getResponses = (responses: Record<number, AppRouteResponse>) => {
+  return Object.entries(responses).reduce(
+    (acc, [statusCode, responseSchema]) => {
+      const description =
+        isZodType(responseSchema) && responseSchema.description
+          ? responseSchema.description
+          : statusCode;
+
+      const httpSuccessCodePattern: RegExp = /^2[0-9]{2}$/;
+      const isSuccess = httpSuccessCodePattern.test(statusCode);
+      const keyMediaObject = isSuccess
+        ? "application/json"
+        : "application/problem+json";
+
+      return {
+        ...acc,
+        [statusCode]: {
+          description,
+          ...(responseSchema
+            ? {
+                content: {
+                  [keyMediaObject]: {
+                    schema: responseSchema,
+                  },
+                },
+              }
+            : {}),
+        },
+      };
+    },
+    {}
+  );
+};
+
+export const getPathsFromRouter = (
+  router: AppRouter,
+  pathHistory?: string[]
+): RouterPath[] => {
+  const paths: RouterPath[] = [];
+
+  Object.keys(router).forEach((key) => {
+    const value = router[key];
+
+    if (isAppRoute(value)) {
+      const pathWithPathParams = value.path.replace(/:(\w+)/g, "{$1}");
+
+      paths.push({
+        id: key,
+        path: pathWithPathParams,
+        route: value,
+        paths: pathHistory ?? [],
+      });
+    } else {
+      paths.push(...getPathsFromRouter(value, [...(pathHistory ?? []), key]));
+    }
+  });
+
+  return paths;
+};
+
+/**
+ * Differentiate between a route and a router
+ *
+ * @param obj
+ * @returns
+ */
+export const isAppRoute = (obj: AppRoute | AppRouter): obj is AppRoute => {
+  return "method" in obj && "path" in obj;
+};
