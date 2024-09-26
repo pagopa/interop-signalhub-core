23027e798ba748a09a8dc5ee6d63a7c9972af92d
borgesis95 <salvatore.borgesi@pagopa.it>
2024-09-26 10:44:02 +0200

    fix: updated openAPI spec's names

--- packages/push-signal/src/commands/generate-api.command.ts
+++ packages/push-signal/src/commands/generate-api.command.ts
@@ -57,6 +57,6 @@ export function generateApi(version: string): void {
     components: document.components,
   };
 
-  const fileOutputDocument = `./src/api/push-signals_${openApiDocument.info.version}_.yaml`;
+  const fileOutputDocument = `./src/api/push-signals_${openApiDocument.info.version}.yaml`;
   writeFileSync(fileOutputDocument, yaml.dump(openApiDocument));
 }
