d27ec9dfcf27c215e605f424e51be645f2ab003e
borgesis95 <salvatore.borgesi@pagopa.it>
2024-09-30 15:52:04 +0200

    refactor: removed kafka-producer package

--- packages/kafka-producer/src/eservices/index.ts
+++
@@ -1,8 +0,0 @@
-import {
-  EServiceEvent,
-  encodeOutboundEServiceEvent,
-} from "@pagopa/interop-outbound-models";
-
-export function produceEserviceEvent(eserviceEvent: EServiceEvent): string {
-  return encodeOutboundEServiceEvent(eserviceEvent);
-}
