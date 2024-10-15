fd4c112eb494ec4711a1062e8bf36c9f42ccb02b
borgesis95 <salvatore.borgesi@pagopa.it>
2024-10-15 14:41:26 +0200

    chore: removed findBy from interop.repository

--- packages/push-signal/src/repositories/interop.repository.ts
+++ packages/push-signal/src/repositories/interop.repository.ts
@@ -7,20 +7,10 @@ export interface IInteropRepository {
     producerId: string,
     state: string
   ) => Promise<string | null>;
-
-  findBy: (
-    eserviceId: string,
-    purposeId: string,
-    purposeState: string,
-    eserviceState: string
-  ) => Promise<{
-    eservice?: { id: string; state: string; producerId: string };
-  } | null>;
 }
 
 export const interopRepository = (db: DB): IInteropRepository => {
   const eserviceTable: TableName = `${config.interopSchema}.eservice`;
-  const purposeTable: TableName = `${config.interopSchema}.purpose`;
   return {
     async findByEserviceIdAndProducerId(
       eserviceId: string,
@@ -36,23 +26,6 @@ export const interopRepository = (db: DB): IInteropRepository => {
         throw genericError(
           `Error interopRepository::findByEserviceIdAndProducerId ${error}`
         );
-      }
-    },
-    async findBy(
-      eserviceId: string,
-      purposeId: string,
-      purposeState: string,
-      eserviceState: string
-    ): Promise<{
-      eservice?: { id: string; state: string; producerId: string };
-    } | null> {
-      try {
-        return await db.oneOrNone(
-          `select eservice.eservice_id, eservice.state, eservice.producer_id from ${purposeTable} purpose, ${eserviceTable} eservice where purpose.consumer_id = eservice.producer_id and eservice.eservice_id = $1 and purpose.purpose_id = $2 and UPPER(purpose.purpose_state) = UPPER($3) and UPPER(eservice.state) = UPPER($4)`,
-          [eserviceId, purposeId, purposeState, eserviceState]
-        );
-      } catch (error: unknown) {
-        throw genericError(`Error interopRepository::findBy ${error}`);
       }
     },
   };
