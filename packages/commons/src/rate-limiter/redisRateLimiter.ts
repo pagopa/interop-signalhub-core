4448670fa82a1c2bf5dfb62cf94e5a937906a6eb
borgesis95 <salvatore.borgesi@pagopa.it>
2025-01-21 09:28:58 +0100

    fix: update pattern of rate-limiter's log

--- packages/commons/src/rate-limiter/redisRateLimiter.ts
+++ packages/commons/src/rate-limiter/redisRateLimiter.ts
@@ -82,7 +82,7 @@ export async function initRedisRateLimiter(config: {
       return match(error)
         .with(P.instanceOf(RateLimiterRes), (rejRes) => {
           logger.warn(
-            `Rate Limit triggered for rateLimiterKey: ${rateLimiterKey}`
+            `[rate-limiter]: Rate-limit triggered for rateLimiterKey: ${rateLimiterKey}`
           );
           return {
             limitReached: true,
