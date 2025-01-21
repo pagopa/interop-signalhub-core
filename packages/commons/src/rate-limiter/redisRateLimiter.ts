67cba5aebd1adb5262b2ab4ab8870b4f77b88ed0
borgesis95 <salvatore.borgesi@pagopa.it>
2025-01-21 09:47:22 +0100

    fix: upper case string rate-limiter into log

--- packages/commons/src/rate-limiter/redisRateLimiter.ts
+++ packages/commons/src/rate-limiter/redisRateLimiter.ts
@@ -82,7 +82,7 @@ export async function initRedisRateLimiter(config: {
       return match(error)
         .with(P.instanceOf(RateLimiterRes), (rejRes) => {
           logger.warn(
-            `[rate-limiter]: Rate-limit triggered for rateLimiterKey: ${rateLimiterKey}`
+            `[RATE-LIMITER]: Rate-limit triggered for rateLimiterKey: ${rateLimiterKey}`
           );
           return {
             limitReached: true,
