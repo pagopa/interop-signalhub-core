3c2a8a7d122a075b8cf0a16d9d6531880d5e0026
borgesis95 <salvatore.borgesi@pagopa.it>
2025-02-06 11:13:29 +0100

    Align develop with main (20250602) (#199)
    Co-authored-by: Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>
    Co-authored-by: Paolo Manca <paolo.manca@pagopa.it>

--- packages/pull-signal/src/config/env.ts
+++ packages/pull-signal/src/config/env.ts
@@ -10,7 +10,25 @@ import { z } from "zod";
 const PullServiceConfig = HTTPServerConfig.and(SignalHubStoreConfig)
   .and(JWTConfig)
   .and(APIServiceConfig)
-  .and(RedisRateLimiterConfig);
+  .and(RedisRateLimiterConfig)
+  .and(
+    z
+      .object({
+        FEATURE_FLAG_SIGNALHUB_WHITELIST: z
+          .enum(["true", "false"])
+          .default("false")
+          .transform((value) => value === "true"),
+        SIGNALHUB_WHITELIST: z
+          .string()
+          .transform((value) => value.split(","))
+          .pipe(z.array(z.string().uuid()))
+          .optional()
+      })
+      .transform((c) => ({
+        featureFlagSignalhubWhitelist: c.FEATURE_FLAG_SIGNALHUB_WHITELIST,
+        signalhubWhitelist: c.SIGNALHUB_WHITELIST
+      }))
+  );
 
 export type PullServiceConfig = z.infer<typeof PullServiceConfig>;
 
