d4252610ca492c32c8409bffa155fb21eed2058f
borgesis95 <salvatore.borgesi@pagopa.it>
2025-04-08 10:46:18 +0200

    feat: added time window on pull signals

--- packages/pull-signal/src/services/signal.service.ts
+++ packages/pull-signal/src/services/signal.service.ts
@@ -1,5 +1,11 @@
-import { DB, Logger, SignalResponse } from "pagopa-signalhub-commons";
+import {
+  DB,
+  Logger,
+  SignalRecord,
+  SignalResponse
+} from "pagopa-signalhub-commons";
 
+import { config } from "../config/env.js";
 import { toSignalResponse } from "../model/domain/toSignalResponse.js";
 import { signalRepository } from "../repositories/signal.repository.js";
 
@@ -30,14 +36,29 @@ export function signalServiceBuilder(db: DB): ISignalService {
       logger.info(
         `SignalService::getSignal, signalId: ${signalId}, limit: ${limit}`
       );
-      const records = await signalRepository(db).getByEservice(
-        eserviceId,
-        signalId,
-        limit
-      );
-      const signals: SignalResponse[] = (records || []).map((record) =>
-        toSignalResponse(record)
-      );
+
+      // eslint-disable-next-line functional/no-let
+      let records: SignalRecord[] | null = [];
+
+      if (config.featureFlagTimeWindow) {
+        logger.debug(
+          `SignalService::getSignal with config FEATURE_FLAG_TIME_WINDOW = true, timeWindowInSeconds: ${config.timeWindowInSeconds} seconds`
+        );
+        records = await signalRepository(db).getByEserviceByTimeWindow(
+          eserviceId,
+          signalId,
+          limit,
+          config.timeWindowInSeconds
+        );
+      } else {
+        records = await signalRepository(db).getByEservice(
+          eserviceId,
+          signalId,
+          limit
+        );
+      }
+
+      const signals: SignalResponse[] = (records || []).map(toSignalResponse);
       const lastSignalId = signals.length
         ? signals[signals.length - 1].signalId
         : null;
