dd7d08f1f22025eb69ee71947879ab63e0a30ca7
borgesis95 <salvatore.borgesi@pagopa.it>
2025-04-10 09:04:57 +0200

    Fix/PIN-6433: fix query params (default) (#213)

--- packages/pull-signal/src/routes/pull.route.ts
+++ packages/pull-signal/src/routes/pull.route.ts
@@ -21,15 +21,16 @@ export const pullRoutes = (
   });
   const pullSignal: AppRouteImplementation<
     typeof contract.pullSignal
-  > = async ({ req }) => {
+  > = async ({ req, query, params }) => {
     const log = logger({
       serviceName: req.ctx.serviceName,
       correlationId: req.ctx.correlationId
     });
+
     try {
-      const { eserviceId } = req.params;
+      const { eserviceId } = params;
       const { organizationId } = req.ctx.sessionData;
-      const { signalId, size } = req.query;
+      const { signalId, size } = query;
 
       log.info(`Pulling signals for e-service ${eserviceId}`);
 
