dd82634f592e5fc77581a6c160b869e00a6b4525
Paolo Manca <paolo.manca@pagopa.it>
2025-05-21 21:48:49 +0200

    addin test

--- packages/push-signal/test/signalService.test.ts
+++ packages/push-signal/test/signalService.test.ts
@@ -10,25 +10,29 @@ import {
 import {
   cleanup,
   getRandomInt,
+  withinTimeWindow as insideTimeWindow,
+  outSideTimeWindow as outsideTimeWindow,
   postgresDB,
   signalService,
   sleep
 } from "./utils.js";
 
 describe("Store service", () => {
+  beforeEach(cleanup);
   describe("verifySignalDuplicated", () => {
-    beforeEach(cleanup);
     it("If signal not exist on db should not throw an error", async () => {
       const signalId = 1;
       const eserviceId = "test-eservice-id";
+
       await expect(
         signalService.verify(signalId, eserviceId, genericLogger)
       ).resolves.not.toThrow();
     });
 
     it("If signal already exist on db should throw a signalIdDuplicatedForEserviceId error", async () => {
-      const signalId = 1;
+      const signalId = getRandomInt();
       const eserviceId = "test-eservice-id";
+
       const signal = createSignal({ signalId, eserviceId });
       await writeSignal(signal, postgresDB, config.signalHubSchema);
 
@@ -38,31 +42,27 @@ describe("Store service", () => {
         signalIdDuplicatedForEserviceId(signalId, eserviceId)
       );
     });
-
-    it("Should be able to deposit signal with higher signalId if the signalId is not consolidated yet", async () => {
-      const firstSignalId = 3;
-      const eserviceId = "test-eservice-id";
-
-      const firstSignal = createSignal({ signalId: firstSignalId, eserviceId });
-      await writeSignal(firstSignal, postgresDB, config.signalHubSchema);
-
-      const TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL =
-        config.timeWindowInSeconds * 1000 - 500;
-
-      await sleep(TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL);
-
-      const secondSignalId = firstSignalId - 1;
-      await expect(
-        signalService.verify(secondSignalId, eserviceId, genericLogger)
-      ).resolves.not.toThrow();
-    });
   });
 
   describe("Push signal with time windows disabled", () => {
     beforeAll(() => {
       config.featureFlagTimeWindow = false;
     });
-    it("Should NOT throw an error if the signalId is already stored on the db and signalId is lower than the last signalId", async () => {
+
+    it("Should be able to deposit signal if the signalId is higher than the last stored signalId", async () => {
+      const firstSignalId = getRandomInt();
+      const eserviceId = "test-eservice-id";
+
+      const firstSignal = createSignal({ signalId: firstSignalId, eserviceId });
+      await writeSignal(firstSignal, postgresDB, config.signalHubSchema);
+
+      const secondSignalId = firstSignalId + 1;
+      await expect(
+        signalService.verify(secondSignalId, eserviceId, genericLogger)
+      ).resolves.not.toThrow();
+    });
+
+    it("Should be able to deposit signal if the signalId is lower than the last stored signalId", async () => {
       const firstSignalId = getRandomInt();
       const eserviceId = "test-eservice-id";
 
@@ -81,15 +81,30 @@ describe("Store service", () => {
       config.featureFlagTimeWindow = true;
       config.timeWindowInSeconds = 5;
     });
-    it("Should throw an error if the signalId is already stored on the db and signalId is lower than the last signalId", async () => {
+
+    it("Should be able to deposit signal if the signalId is higher than the last stored signalId", async () => {
       const firstSignalId = getRandomInt();
       const eserviceId = "test-eservice-id";
 
       const firstSignal = createSignal({ signalId: firstSignalId, eserviceId });
       await writeSignal(firstSignal, postgresDB, config.signalHubSchema);
 
-      const TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL =
-        config.timeWindowInSeconds * 1000 + 500;
+      const secondSignalId = firstSignalId + 1;
+      await expect(
+        signalService.verify(secondSignalId, eserviceId, genericLogger)
+      ).resolves.not.toThrow();
+    });
+
+    it("Should NOT be able to deposit signal if the signalId is lower than the last stored signalI outside the time window", async () => {
+      const firstSignalId = getRandomInt();
+      const eserviceId = "test-eservice-id";
+
+      const firstSignal = createSignal({ signalId: firstSignalId, eserviceId });
+      await writeSignal(firstSignal, postgresDB, config.signalHubSchema);
+
+      const TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL = outsideTimeWindow(
+        config.timeWindowInSeconds
+      );
       await sleep(TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL);
 
       const secondSignalId = firstSignalId - 1;
@@ -98,6 +113,24 @@ describe("Store service", () => {
       ).rejects.toThrowError(
         signalStoredWithHigherSignalId(secondSignalId, eserviceId)
       );
+    });
+
+    it("Should be able to deposit signal if the signalId is lower than the last stored signalId inside the time window", async () => {
+      const firstSignalId = getRandomInt();
+      const eserviceId = "test-eservice-id";
+
+      const firstSignal = createSignal({ signalId: firstSignalId, eserviceId });
+      await writeSignal(firstSignal, postgresDB, config.signalHubSchema);
+
+      const TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL = insideTimeWindow(
+        config.timeWindowInSeconds
+      );
+      await sleep(TIME_TO_WAIT_BEFORE_DEPOSIT_SIGNAL);
+
+      const secondSignalId = firstSignalId - 1;
+      await expect(
+        signalService.verify(secondSignalId, eserviceId, genericLogger)
+      ).resolves.not.toThrow();
     });
   });
 });
--- packages/push-signal/test/utils.ts
+++ packages/push-signal/test/utils.ts
@@ -4,6 +4,7 @@ import {
 } from "pagopa-signalhub-commons-test";
 import { inject } from "vitest";
 
+import { PushServiceConfig } from "../src/config/env.js";
 import { interopServiceBuilder } from "../src/services/interop.service.js";
 import { queueServiceBuilder } from "../src/services/queque.service.js";
 import { signalServiceBuilder } from "../src/services/signal.service.js";
@@ -36,4 +37,12 @@ export function getRandomInt(min = 1, max = 100): number {
   const lower = Math.ceil(min);
   const upper = Math.floor(max);
   return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+export function outSideTimeWindow(timeWindowInSeconds: number) {
+  return timeWindowInSeconds * 1000 + 500;
+}
+
+export function withinTimeWindow(timeWindowInSeconds: number) {
+  return timeWindowInSeconds * 1000 - 500;
 }
